6612cbacb3a0812559a6deb31fbf5303d54e3a23
Дмитрий Сергиенко <dasergienko@edu.hse.ru>
2024-07-11 21:05:59 +0300

    Solve homework 3

---
+++ Homework3/cmd/grpc/client/main.go
@@ -0,0 +1,210 @@
+package main
+
+import (
+	"awesomeProject/proto"
+	"context"
+	"flag"
+	"fmt"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"time"
+)
+
+type Command struct {
+	Port    int
+	Host    string
+	Cmd     string
+	Name    string
+	NewName string
+	Amount  int
+}
+
+func (c *Command) Do() error {
+	switch c.Cmd {
+	case "create":
+		if err := c.create(); err != nil {
+			return fmt.Errorf("create account failed: %w", err)
+		}
+
+		return nil
+	case "get":
+		if err := c.get(); err != nil {
+			return fmt.Errorf("get account failed: %w", err)
+		}
+
+		return nil
+	case "delete":
+		if err := c.delete(); err != nil {
+			return fmt.Errorf("delete account failed: %w", err)
+		}
+
+		return nil
+	case "change":
+		if err := c.changeAccount(); err != nil {
+			return fmt.Errorf("change account failed: %w", err)
+		}
+
+		return nil
+	case "path":
+		if err := c.pathAccount(); err != nil {
+			return fmt.Errorf("path account failed: %w", err)
+		}
+		return nil
+
+	default:
+		return fmt.Errorf("unknown command %s", c.Cmd)
+	}
+}
+
+func (c *Command) create() error {
+	conn, err := grpc.NewClient("0.0.0.0:4567", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return fmt.Errorf("create grpc client failed: %w", err)
+	}
+	defer func() {
+		_ = conn.Close()
+
+	}()
+	con := proto.NewBankClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	res, err := con.CreateAccountRequest(ctx, &proto.CreateAccount{Name: c.Name, Balance: int64(c.Amount)})
+	if err != nil {
+		return fmt.Errorf("create account failed: %w", err)
+	}
+	if res.Status == "" {
+		res.Status = "ok"
+	}
+	fmt.Println("status:", res.Status)
+	return nil
+}
+
+func (c *Command) get() error {
+	conn, err := grpc.NewClient("0.0.0.0:4567", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return fmt.Errorf("get grpc client failed: %w", err)
+	}
+	defer func() {
+		_ = conn.Close()
+
+	}()
+	con := proto.NewBankClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	res, err := con.GetAccountResponse(ctx, &proto.GetName{Name: c.Name})
+	if err != nil {
+		return fmt.Errorf("create account failed: %w", err)
+	}
+	if res.Status == "" {
+		res.Status = "ok"
+	} else {
+		fmt.Println("status:", res.Status)
+		return nil
+	}
+	fmt.Println("status:", res.Status)
+	fmt.Println("balance:", res.Balance)
+	return nil
+}
+
+func (c *Command) delete() error {
+	conn, err := grpc.NewClient("0.0.0.0:4567", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return fmt.Errorf("delete grpc client failed: %w", err)
+	}
+	defer func() {
+		_ = conn.Close()
+
+	}()
+	con := proto.NewBankClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	res, err := con.DeleteAccountRequest(ctx, &proto.DeleteAccount{Name: c.Name})
+	if err != nil {
+		return fmt.Errorf("delete account failed: %w", err)
+	}
+	if res.Status == "" {
+		res.Status = "ok"
+	}
+	fmt.Println("status:", res.Status)
+	return nil
+}
+
+func (c *Command) changeAccount() error {
+	conn, err := grpc.NewClient("0.0.0.0:4567", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return fmt.Errorf("change grpc client failed: %w", err)
+	}
+	defer func() {
+		_ = conn.Close()
+
+	}()
+	con := proto.NewBankClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	res, err := con.ChangeAccountRequest(ctx, &proto.ChangeAccount{Name: c.Name, NewName: c.NewName})
+	if err != nil {
+		return fmt.Errorf("change account failed: %w", err)
+	}
+	if res.Status == "" {
+		res.Status = "ok"
+	}
+	fmt.Println("status:", res.Status)
+	return nil
+}
+
+func (c *Command) pathAccount() error {
+	conn, err := grpc.NewClient("0.0.0.0:4567", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return fmt.Errorf("path grpc client failed: %w", err)
+	}
+	defer func() {
+		_ = conn.Close()
+
+	}()
+	con := proto.NewBankClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	res, err := con.PathAccountRequest(ctx, &proto.PathAccount{Name: c.Name, Balance: int64(c.Amount)})
+	if err != nil {
+		return fmt.Errorf("path account failed: %w", err)
+	}
+	if res.Status == "" {
+		res.Status = "ok"
+	}
+	fmt.Println("status:", res.Status)
+	return nil
+}
+
+func main() {
+	portVal := flag.Int("port", 8080, "server port")
+	hostVal := flag.String("host", "0.0.0.0", "server host")
+	cmdVal := flag.String("cmd", "", "command to execute")
+	nameVal := flag.String("name", "", "name of account")
+	amountVal := flag.Int("amount", 0, "amount of account")
+	newName := flag.String("new_name", "", "new name of account")
+
+	flag.Parse()
+
+	cmd := Command{
+		Port:    *portVal,
+		Host:    *hostVal,
+		Cmd:     *cmdVal,
+		Name:    *nameVal,
+		Amount:  *amountVal,
+		NewName: *newName,
+	}
+
+	if err := cmd.Do(); err != nil {
+		panic(err)
+	}
+}
---
+++ Homework3/cmd/grpc/server/main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"awesomeProject/proto"
+	"context"
+	"fmt"
+	"sync"
+
+	"google.golang.org/grpc"
+	"net"
+)
+
+type server struct {
+	proto.UnimplementedBankServer
+}
+
+type Handler struct {
+	accounts map[string]*proto.Account
+	guard    *sync.RWMutex
+}
+
+var h Handler
+
+func (s *server) DeleteAccountRequest(ctx context.Context, req *proto.DeleteAccount) (*proto.Status, error) {
+	h.guard.Lock()
+	if _, ok := h.accounts[req.Name]; !ok {
+		return &proto.Status{Status: "account does not exist"}, nil
+	}
+	delete(h.accounts, req.Name)
+	h.guard.Unlock()
+	return &proto.Status{Status: ""}, nil
+}
+
+func (s *server) PathAccountRequest(ctx context.Context, req *proto.PathAccount) (*proto.Status, error) {
+	h.guard.Lock()
+	if _, ok := h.accounts[req.Name]; !ok {
+		h.guard.Unlock()
+		return &proto.Status{Status: "account does not exist"}, nil
+	}
+	h.accounts[req.Name] = &proto.Account{Name: req.Name, Balance: req.Balance}
+	h.guard.Unlock()
+	return &proto.Status{Status: ""}, nil
+}
+
+func (s *server) ChangeAccountRequest(ctx context.Context, req *proto.ChangeAccount) (*proto.Status, error) {
+	h.guard.Lock()
+	account, ok := h.accounts[req.Name]
+	if !ok {
+		h.guard.Unlock()
+		return &proto.Status{Status: "account does not exist"}, nil
+	}
+	h.accounts[req.Name] = &proto.Account{Name: req.NewName, Balance: account.Balance}
+	h.guard.Unlock()
+	return &proto.Status{Status: ""}, nil
+}
+
+func (s *server) CreateAccountRequest(ctx context.Context, req *proto.CreateAccount) (*proto.Status, error) {
+	h.guard.Lock()
+	h.accounts[req.Name] = &proto.Account{Name: req.Name, Balance: req.Balance}
+	h.guard.Unlock()
+	return &proto.Status{Status: ""}, nil
+}
+
+func (s *server) GetAccountResponse(ctx context.Context, req *proto.GetName) (*proto.Balance, error) {
+	h.guard.Lock()
+	account, ok := h.accounts[req.Name]
+	if !ok {
+		h.guard.Unlock()
+		return &proto.Balance{Balance: 0, Status: "account does not exist"}, nil
+	}
+	h.guard.Unlock()
+	return &proto.Balance{Balance: account.Balance, Status: ""}, nil
+}
+
+func main() {
+	h.guard = new(sync.RWMutex)
+	h.accounts = make(map[string]*proto.Account)
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", 4567))
+	if err != nil {
+		panic(err)
+	}
+	s := grpc.NewServer()
+	proto.RegisterBankServer(s, &server{})
+	if err := s.Serve(lis); err != nil {
+		panic(err)
+	}
+
+}
